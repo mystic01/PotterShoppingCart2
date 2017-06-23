67dd8239d8c18a0a8b1aa8c7ad196f35182f3a47
Mystic <mysteriousGE@gmail.com>
2017-06-23 23:13:43 +0800

    第七個綠燈_第一集1本_第二集2本_第三集2本_預計回傳460

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -122,5 +122,24 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculateTotalPriceTest_第一集1本_第二集2本_第三集2本_預計回傳460()
+        {
+            //Arrange
+            var books = new Dictionary<string, int>()
+            {
+                { "哈利波特_1", 1},
+                { "哈利波特_2", 2},
+                { "哈利波特_3", 2},
+            };
+            var expected = 460;
+
+            //Act
+            var actual = PotterShoppingCart.CalculateTotalPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
