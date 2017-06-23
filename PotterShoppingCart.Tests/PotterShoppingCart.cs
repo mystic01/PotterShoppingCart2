82f66b90c2d80c6576229ca5a37e2ac36f27fab1
Mystic <mysteriousGE@gmail.com>
2017-06-23 23:23:24 +0800

    嘗試提高可讀性

--- PotterShoppingCart.Tests/PotterShoppingCart.cs
+++ PotterShoppingCart.Tests/PotterShoppingCart.cs
@@ -7,39 +7,37 @@ namespace PotterShoppingCart.Tests
 {
     public class PotterShoppingCart
     {
+        private const int BOOK_PRICE = 100;
+        private static readonly Dictionary<int, double> SUITE_DISCOUNT = new Dictionary<int, double>
+        {
+            {1, 0},
+            {2, 0.05},
+            {3, 0.1},
+            {4, 0.2},
+            {5, 0.25}
+        };
+
         public static int CalculateTotalPrice(Dictionary<string, int> books)
         {
-            const int bookPrice = 100;
             var totalPrice = 0;
             
             while (books.Count > 0)
             {
-                var discount = GetDiscountBySuiteSize(books.Count);
-                totalPrice += (int) (bookPrice * books.Count * (1 - discount));
+                var suiteSize = books.Count;
+                var suiteAmout = books.Min(x => x.Value);
+
+                var discount = SUITE_DISCOUNT[suiteSize];                
+                var oneSuitePrice = (int)(BOOK_PRICE * suiteSize * (1 - discount));
+
+                totalPrice += oneSuitePrice * suiteAmout;
 
                 //算過的從 books 中移掉
-                books = books.ToDictionary(x => x.Key, x => x.Value - 1)
+                books = books.ToDictionary(x => x.Key, x => x.Value - suiteAmout)
                     .Where(x => x.Value > 0)
                     .ToDictionary(x => x.Key, x => x.Value);
             }
 
             return totalPrice;
-        }
-
-        private static double GetDiscountBySuiteSize(int suiteSize)
-        {
-            var discount = 0.0;
-            if (suiteSize == 1)
-                discount = 0.0;
-            else if (suiteSize == 2)
-                discount = 0.05;
-            else if (suiteSize == 3)
-                discount = 0.1;
-            else if (suiteSize == 4)
-                discount = 0.2;
-            else if (suiteSize >= 5)
-                discount = 0.25;
-            return discount;
         }
     }
 }
