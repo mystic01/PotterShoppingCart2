db16a6d7a56fdf5e91f94a2207201823abc454bb
Mystic <mysteriousGE@gmail.com>
2017-06-23 22:40:29 +0800

    第三個紅燈_第一集1本_第二集1本_第三集1本_預計回傳270

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -43,5 +43,24 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculateTotalPriceTest_第一集1本_第二集1本_第三集1本_預計回傳270()
+        {
+            //Arrange
+            var books = new Dictionary<string, int>()
+            {
+                { "哈利波特_1", 1},
+                { "哈利波特_2", 1},
+                { "哈利波特_3", 1}
+            };
+            var expected = 270;
+
+            //Act
+            var actual = PotterShoppingCart.CalculateTotalPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
