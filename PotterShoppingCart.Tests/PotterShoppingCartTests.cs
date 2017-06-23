9e4dcac43fe276121f08dad7f7156de1c3814a71
Mystic <mysteriousGE@gmail.com>
2017-06-23 22:32:05 +0800

    第一個紅燈_第一集1本_預計回傳100

---
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void CalculateTotalPriceTest_第一集1本_預計回傳100()
+        {
+            //Arrange
+            var books = new Dictionary<string, int>()
+            {
+                { "哈利波特_1", 1}
+            };
+            var expected = 100;
+
+            //Act
+            var actual = PotterShoppingCart.CalculateTotalPrice(books);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
