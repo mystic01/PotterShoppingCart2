860ce20ba61123786eafd9aaeeb2d28cc3b93835
Mystic <mysteriousGE@gmail.com>
2017-06-23 23:11:35 +0800

    第六個綠燈_第一集1本_第二集1本_第三集2本_預計回傳370

--- PotterShoppingCart.Tests/PotterShoppingCart.cs
+++ PotterShoppingCart.Tests/PotterShoppingCart.cs
@@ -10,18 +10,36 @@ namespace PotterShoppingCart.Tests
         public static int CalculateTotalPrice(Dictionary<string, int> books)
         {
             const int bookPrice = 100;
+            var totalPrice = 0;
+            
+            while (books.Count > 0)
+            {
+                var discount = GetDiscountBySuiteSize(books.Count);
+                totalPrice += (int) (bookPrice * books.Count * (1 - discount));
+
+                //算過的從 books 中移掉
+                books = books.ToDictionary(x => x.Key, x => x.Value - 1)
+                    .Where(x => x.Value > 0)
+                    .ToDictionary(x => x.Key, x => x.Value);
+            }
+
+            return totalPrice;
+        }
+
+        private static double GetDiscountBySuiteSize(int suiteSize)
+        {
             var discount = 0.0;
-
-            if (books.Count == 2)
+            if (suiteSize == 1)
+                discount = 0.0;
+            else if (suiteSize == 2)
                 discount = 0.05;
-            else if (books.Count == 3)
+            else if (suiteSize == 3)
                 discount = 0.1;
-            else if (books.Count == 4)
+            else if (suiteSize == 4)
                 discount = 0.2;
-            else if (books.Count >= 5)
+            else if (suiteSize >= 5)
                 discount = 0.25;
-
-            return (int) (bookPrice * books.Count * (1-discount));
+            return discount;
         }
     }
 }
